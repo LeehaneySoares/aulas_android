815655e638390e047e79303080c2e7e64b75e1f2
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-04-29 22:31:06 -0300

    chore(java.MainActivity): adicionando meu primeiro evento de click

--- myfirstapp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ myfirstapp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void changeText (View view){
+    TextView text = findViewById(R.id.textTittle);
+    text.setText("Hello, world!");
   }
 }
